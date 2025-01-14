6a133694a69d6aa7888f7d5e7c62354b000b5412
PeterC <petercalifano.gs@gmail.com>
2025-01-14 16:29:09 +0100

    Add placeholder file

---
+++ src/attitude/CAttitudePointingGenerator.h
@@ -0,0 +1,18 @@
+
+#pragma once
+// TODO (PC) implement code adapting MATLAB  CAttitudePointingGenerator class
+namespace attitude
+{
+    enum class EnumAttitudePointingMethod
+    {
+
+    };
+
+    class CAttitudePointingGenerator
+    {
+      public:
+        CAttitudePointingGenerator();
+        ~CAttitudePointingGenerator();
+    };
+
+} // namespace attitude
