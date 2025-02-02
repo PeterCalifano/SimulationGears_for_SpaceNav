53513a312c285435737983e3bc699ccfe1180f9a
PeterC <petercalifano.gs@gmail.com>
2025-02-02 16:37:16 +0100

    Implement prototype of full RT visibility check for predetermined points (mesh occlusion + shadow ray)

--- matlab/simulation_models/sensors/cameras/RayTracePointVisibility_ShadowRays.m
+++ matlab/simulation_models/sensors/cameras/RayTracePointVisibility_ShadowRays.m
@@ -1,17 +1,15 @@
 function [bAllPointsVisibilityMask, dProjectedPoints_UV] = RayTracePointVisibility_ShadowRays(ui32PointsIdx, ...
-                                                                                                 dPointsPositions_TB, ...
-                                                                                                 strTargetBodyData, ...
-                                                                                                 strCameraData, ...
-                                                                                                 dSunDir_TB, ...
-                                                                                                 strFcnOptions, ...
-                                                                                                 bDEBUG_MODE) %#codegen
+                                                                                               dPointsPositions_TB, ...
+                                                                                               strTargetBodyData, ...
+                                                                                               strCameraData, ...
+                                                                                               dSunPosition_TB, ...
+                                                                                               bDEBUG_MODE) %#codegen
 arguments
     ui32PointsIdx       (1,:) uint32
     dPointsPositions_TB (3,:) double
     strTargetBodyData   {isstruct}
     strCameraData       {isstruct} 
-    dSunDir_TB          (3,1) double
-    strFcnOptions       {isstruct}
+    dSunPosition_TB     (3,1) double
     bDEBUG_MODE         (1,1) logical {islogical} = false
 end
 %% PROTOTYPE
@@ -31,7 +29,7 @@ end
 % dPointsPositions_TB (3,:) double
 % strTargetBodyData   {isstruct}
 % strCameraData       {isstruct}
-% dSunDir_TB          (3,1) double
+% dSunPosition_TB      (3,1) double
 % strFcnOptions       {isstruct}
 % bDEBUG_MODE         (1,1) logical {islogical} = false
 % -------------------------------------------------------------------------------------------------------------
@@ -51,18 +49,6 @@ end
 % -------------------------------------------------------------------------------------------------------------
 
 % Defaults
-if not(isfield(strFcnOptions, 'bENABLE_HEUR_GEOM_FEAS_CHECK'))
-    strFcnOptions.bENABLE_HEUR_GEOM_FEAS_CHECK = false;
-end
-
-if not(isfield(strFcnOptions, 'dIllumAngleThr'))
-    strFcnOptions.dIllumAngleThr = deg2rad(40); % Valid phase angles: 180-illumAngleThr
-end
-
-if not(isfield(strFcnOptions, 'dLosAngleThr'))
-    strFcnOptions.dLosAngleThr = deg2rad(40); % Valid angles: 180-dLosAngleThr
-end
-
 
 % TARGET BODY
 assert(size(strTargetBodyData.strShapeModel.ui32triangVertexPtr, 1) == 3, "ERROR: strShapeModel.ui32triangVertexPtr must have [3xN] shape.")
@@ -75,7 +61,7 @@ dDCM_INfromTB  = strTargetBodyData.dDCM_INfromTB;
 dDCM_INfromCAM = strCameraData.dDCM_INfromCAM;
 
 % CAMERA
-dPosition_IN = strCameraData.dPosition_IN; % TO MODIFY
+dCamPosition_IN = strCameraData.dPosition_IN; % TO MODIFY
 dResX        = strCameraData.dResX;
 dResY        = strCameraData.dResY;
 
@@ -86,21 +72,14 @@ assert(size(dPointsPositions_TB, 2) == length(ui32PointsIdx), "ERROR: number of 
 % dDCM_TBfromCAM = transpose(dDCM_INfromTB) * dDCM_INfromCAM;
 % dDCM_fromTBtoCAM = dDCM_TBfromCAM';
 
-% Ensure Sun direction is normalized
-if any(abs(dSunDir_TB) > 1.0, 'all')
-    dSunDir_TB = dSunDir_TB./norm(dSunDir_TB);
-end
+% dSunDir_TB = dSunPosition_TB./norm(dSunPosition_TB);
 
 % Compute camera position in target body fixed
-dPosition_TB = transpose(dDCM_INfromTB) * dPosition_IN;
-
-% Compute thresholds values
-dCosIllumAngleThr = cos(strFcnOptions.dIllumAngleThr);
-dCosLosAngleThr   = cos(strFcnOptions.dLosAngleThr);
+dCamPosition_TB = transpose(dDCM_INfromTB) * dCamPosition_IN;
 
 % Get number of triangles and landmarks
 ui32NumOfTriangles = uint32(size( ui32triangVertexPtr, 2));
-i32NumOfPoints     = int32( size(dPointsPositions_TB, 2) );
+% i32NumOfPoints     = int32( size(dPointsPositions_TB, 2) );
 
 % Split array to enable parallelization
 % Position
@@ -108,50 +87,26 @@ dPointPosX_TB = dPointsPositions_TB(1, :);
 dPointPosY_TB = dPointsPositions_TB(2, :);
 dPointPosZ_TB = dPointsPositions_TB(3, :);
 
-% Compute all norms using vectorized operation
-dPointPosNorms = vecnorm(dPointsPositions_TB(1:3, :), 2, 1);
-
-% Compute direction components
-dPointDirX_TB = dPointPosX_TB./dPointPosNorms; 
-dPointDirY_TB = dPointPosY_TB./dPointPosNorms; 
-dPointDirZ_TB = dPointPosZ_TB./dPointPosNorms; 
-
 % Camera position unit vector in TB
-dCameraDir_TB = dPosition_TB/norm(dPosition_TB);
+% dCameraDir_TB = dPosition_TB/norm(dPosition_TB);
 
 % Compute dot product with Sun direction (vectorized)
-% dPointDirDotSunDir_test = dot([dPointDirX_TB; dPointDirY_TB; dPointDirZ_TB], repmat(-dSunDir_TB, 1,length(ui32PointsIdx)), 1);
-dPointDirDotSunDir = sum([dPointDirX_TB; dPointDirY_TB; dPointDirZ_TB]' * (-dSunDir_TB), 2);
-
-% Perform preliminary vectorized checks (get all points that passed checks)
-% Illumination check
-bIlluminationFeasibilityMask = dPointDirDotSunDir <= 0 | dPointDirDotSunDir <= dCosIllumAngleThr;
-
-% TODO (PC): this heuristic check does not grant the points are actually illuminated, because self-shadowing is not checked using RT (shadow rays). Need to do it as optionally enabled in this function. Essentially, for each visible point from the camera, cast a shadow ray to light source and check for intersections. No intersection --> point is visible.
-
-% Heuristic geometrical check
-if strFcnOptions.bENABLE_HEUR_GEOM_FEAS_CHECK == true
-    % Ellipsoid based check for heuristic pruning of points
-    % dPointDirDotCameraPosDir_test = dot(repmat(-dCameraDir_TB, 1,length(ui32PointsIdx)), [dPointDirX_TB; dPointDirY_TB; dPointDirZ_TB], 1);
-    dPointDirDotCameraPosDir = sum([dPointDirX_TB; dPointDirY_TB; dPointDirZ_TB]' * (-dCameraDir_TB), 2);
-    bGeometricalFeasibilityMask = dPointDirDotCameraPosDir < 0 |  abs(dPointDirDotCameraPosDir) <= dCosLosAngleThr;
-else
-    bGeometricalFeasibilityMask = true(i32NumOfPoints, 1);
-end
+% dPointDirDotSunDir_test = dot([dPointDirX_TB; dPointDirY_TB; dPointDirZ_TB], repmat(-dSunPosition_TB, 1,length(ui32PointsIdx)), 1);
+% dPointDirDotSunDir = sum([dPointDirX_TB; dPointDirY_TB; dPointDirZ_TB]' * (-dSunPosition_TB), 2);
 
 % Field of view check
-[dProjectedPoints_UV]   = pinholeProjectArrayHP_DCM(strCameraData.dKcam, dDCM_INfromCAM' * dDCM_INfromTB, dPosition_TB, dPointsPositions_TB);
+[dProjectedPoints_UV] = pinholeProjectArrayHP_DCM(strCameraData.dKcam, dDCM_INfromCAM' * dDCM_INfromTB, dCamPosition_TB, dPointsPositions_TB);
 bPointWithinFoV = ((dProjectedPoints_UV(1, :) > 0 & dProjectedPoints_UV(1, :) < dResX) & (dProjectedPoints_UV(2, :) > 0 & dProjectedPoints_UV(2, :) < dResY))';
 
 % Get mask to extract points requiring RT to check visibility
-bPointsToRayTrace = bPointWithinFoV & bGeometricalFeasibilityMask & bIlluminationFeasibilityMask; 
+bPointsToRayTrace = bPointWithinFoV; 
 
 if bDEBUG_MODE == true
     fprintf("\t Debug print - points visibility check function\n")
-    fprintf("\t Not illuminated: %d\n",                      int32(sum(not(bIlluminationFeasibilityMask))) );
-    fprintf("\t Geometrically unfeasible (heuristic): %d\n", int32(sum(not(bGeometricalFeasibilityMask))) );
-    fprintf("\t Outside field of view: %d\n",                int32(sum(not(bPointWithinFoV))) );
-    fprintf("\t Remaining to ray trace: %d\n",               int32(sum(bPointsToRayTrace)) );
+    %fprintf("\t Not illuminated: %d\n",                      int32(sum(not(bIlluminationFeasibilityMask))) );
+    %fprintf("\t Geometrically unfeasible (heuristic): %d\n", int32(sum(not(bGeometricalFeasibilityMask))) );
+    fprintf("\t Outside field of view: %d\n",      int32(sum(not(bPointWithinFoV))) );
+    fprintf("\t To ray trace: %d\n",               int32(sum(bPointsToRayTrace)) );
 end
 
 % Extract points to check using ray tracing
@@ -162,10 +117,11 @@ dPointPosZ_TB = dPointPosZ_TB(bPointsToRayTrace);
 i32NumOfPointsToTrace = int32( length(dPointPosX_TB) );
 
 % Compute norms of relative positions of points wrt camera
-dRayToPointsFromCam_TB  = [dPointPosX_TB; dPointPosY_TB; dPointPosZ_TB] - dPosition_TB;
+dRayToPointsFromCam_TB  = [dPointPosX_TB; dPointPosY_TB; dPointPosZ_TB] - dCamPosition_TB;
 dRayToPointsFromCamNorm = vecnorm(dRayToPointsFromCam_TB, 2, 1);
 dRayToPointsFromCam_TB  = dRayToPointsFromCam_TB./dRayToPointsFromCamNorm;
 
+% Compute ray directions (to points)
 dPointDirFromCamX_TB = dRayToPointsFromCam_TB(1, :);
 dPointDirFromCamY_TB = dRayToPointsFromCam_TB(2, :);
 dPointDirFromCamZ_TB = dRayToPointsFromCam_TB(3, :);
@@ -179,67 +135,86 @@ i32triangVertPtr3 = ui32triangVertexPtr(3, :);
 bPointsVisibilityMask = false(i32NumOfPointsToTrace, 1); % Initialize visibility mask as "no visibility"
 
 % Visibility check for each landmark (position in TB frame)
-parfor idL = 1:i32NumOfPointsToTrace
+for idL = 1:i32NumOfPointsToTrace
     if bDEBUG_MODE == true 
         if mod(i32NumOfPointsToTrace/idL, 5)
             fprintf("Processing point entry %05d\n", idL)
         end
     end
 
-
     %% Intersection computation checks
     % DEVNOTE: may be modified to select only a subset of the triangles for further optimization
     ui32NumTrianglesInSubset = ui32NumOfTriangles; 
     ui32TrianglesIDsubset = 1:ui32NumOfTriangles;
-    bIsIntersected = false(ui32NumTrianglesInSubset, 1);
-
+    
+    % Loop over triangles (TODO attempt to vectorize code)
     for id = 1:ui32NumTrianglesInSubset
 
         idT = ui32TrianglesIDsubset(id);
         % DEVNOTE the only way to avoid broadcast arrays is to compose first vectors cointaining the indices of the vertices for each triangle.
         % Get triangle vertices ptrs
-        i32triangVertPtr1_tmp = i32triangVertPtr1(id);
+        i32triangVertPtr1_tmp = i32triangVertPtr1(id); % TODO check how to solve broadcasting problem
         i32triangVertPtr2_tmp = i32triangVertPtr2(id);
         i32triangVertPtr3_tmp = i32triangVertPtr3(id);
 
         % Get triangle vertices positions
-        tmpTriangleVertices = coder.nullcopy(zeros(3, 3));
+        dTmpTriangleVertices = coder.nullcopy(zeros(3, 3));
 
         if all(i32triangVertPtr1_tmp ~= ui32PointsIdx(idL) & i32triangVertPtr2_tmp ~= ui32PointsIdx(idL) & ...
                 i32triangVertPtr3_tmp ~= ui32PointsIdx(idL))
 
-            tmpTriangleVertices(1:3, 1) = dVerticesPos(:, i32triangVertPtr1_tmp ); %#ok<PFBNS>
-            tmpTriangleVertices(1:3, 2) = dVerticesPos(:, i32triangVertPtr2_tmp );
-            tmpTriangleVertices(1:3, 3) = dVerticesPos(:, i32triangVertPtr3_tmp );
+            dTmpTriangleVertices(1:3, 1) = dVerticesPos(:, i32triangVertPtr1_tmp ); %#ok<PFBNS>
+            dTmpTriangleVertices(1:3, 2) = dVerticesPos(:, i32triangVertPtr2_tmp );
+            dTmpTriangleVertices(1:3, 3) = dVerticesPos(:, i32triangVertPtr3_tmp );
 
-            % Evaluate intersection through ray tracing
-
-            [bTmpIntersectFlag, ~, ~, dIntersectDistance] = RayTwoSidedTriangleIntersection_MollerTrembore(dPosition_TB, ...
+            % Perform intersection test against mesh (1 ray 1 triangle) using one-sided test 
+            % (cull computations excluding back-facing triangles)
+    
+            [bTmpIntersectFlag, ~, ~, dIntersectDistance] = RayTriangleIntersection_MollerTrumbore(dCamPosition_TB, ...
                 [dPointDirFromCamX_TB(idL); dPointDirFromCamY_TB(idL); dPointDirFromCamZ_TB(idL);],  ... 
-                tmpTriangleVertices(:, 1), tmpTriangleVertices(:, 2), tmpTriangleVertices(:, 3));
+                dTmpTriangleVertices(:, 1), ...
+                dTmpTriangleVertices(:, 2), ...
+                dTmpTriangleVertices(:, 3), ...
+                false, ...
+                false); % Normal ray, one-sided test
+
 
             if (dRayToPointsFromCamNorm(idL) - dIntersectDistance) > eps('single') && bTmpIntersectFlag == true
+                % Intersection closer than point detected --> point occluded by mesh (camera does not see it)
                 assert(idT <= ui32NumOfTriangles)
-                bIsIntersected(idT) = true;
+                bPointsVisibilityMask(idL) = false;
+                break; 
 
-                % TODO (PC) shadow ray casting goes here, to check for intersection with mesh (except for the triangles of which the vertex is part of). Triangles may be excluded easily based on some conditions here.
+            elseif (dRayToPointsFromCamNorm(idL) - dIntersectDistance) <= eps('single') && bTmpIntersectFlag == true
+                % Intersection farther than point detected --> point visible by camera
+                % Cast shadow ray to light and check for illumination occlusion
+                
+                % Compute ray direction (to Sun)
+                dSunDirFromPoint_TB = dSunPosition_TB - [dPointPosX_TB(idL); dPointPosY_TB(idL); dPointPosZ_TB(idL)];
+                dSunDirFromPoint_TB = ndSunDirFromPoint_TB./norm(dSunDirFromPoint_TB);
 
-                break; % Intersection detected --> no need to check other triangles
+                % NOTE: for loop over all triangles of mesh inside (one-sided test)
+                [bLightOcclusion] = TraceShadowRay([dPointPosX_TB(idL); dPointPosY_TB(idL); dPointPosZ_TB(idL)], ...
+                                            dSunDirFromPoint_TB, ...
+                                            dVerticesPos, ...
+                                            ui32TrianglesIDsubset, ...
+                                            ui32NumTrianglesInSubset, ...
+                                            i32triangVertPtr1, ...
+                                            i32triangVertPtr2, ...
+                                            i32triangVertPtr3);
+
+                bPointsVisibilityMask(idL) = not(bLightOcclusion); % Point is visible if light not occluded
+
+            else % bTmpIntersectFlag == false
+                warning(bTmpIntersectFlag, ['No intersection with mesh found for point %s. ...' ...
+                    '\nHowever this should have not occurred because points to check belong to mesh!'], num2str(idL));
+                bPointsVisibilityMask(idL) = false;
+
             end
         end
 
-    end
-
-    if any(bIsIntersected == true, 1)
-        % INTERSECTION --> LM NOT VISIBLE
-    else
-        % NO INTERSECTION --> LM MARKED AS VISIBLE
-        assert(idL <= i32NumOfPoints)
-        bPointsVisibilityMask(idL) = true;
-        % NOTE: Default is false. All selected triangles must be checked to declare visibility
-    end
-
-end
+    end % Loop over triangles
+end % Parallelized Loop over points to trace
 
 if bDEBUG_MODE == true
     ui32HowManyVisLM = sum(bPointsVisibilityMask, "all");
@@ -251,7 +226,60 @@ bAllPointsVisibilityMask = bPointsToRayTrace;
 bAllPointsVisibilityMask(bPointsToRayTrace == 1) = bPointsVisibilityMask;
 dProjectedPoints_UV = dProjectedPoints_UV(:, bAllPointsVisibilityMask);
 
+
 end
 
+% LOCAL FUNCTION for Shadow Ray check
+function [bLightOcclusion] = TraceShadowRay(dRayOrigin, ...
+                                            dRayDirection, ...
+                                            dVerticesPos, ...
+                                            ui32TrianglesIDsubset, ...
+                                            ui32NumTrianglesInSubset, ...
+                                            i32triangVertPtr1, ...
+                                            i32triangVertPtr2, ...
+                                            i32triangVertPtr3)
+
+bLightOcclusion = false;
+% Loop over triangles (TODO attempt to vectorize code)
+for id = 1:ui32NumTrianglesInSubset
+
+    idT = ui32TrianglesIDsubset(id);
+    % DEVNOTE the only way to avoid broadcast arrays is to compose first vectors cointaining the indices of the vertices for each triangle.
+    % Get triangle vertices ptrs
+    i32triangVertPtr1_tmp = i32triangVertPtr1(id); % TODO check how to solve broadcasting problem
+    i32triangVertPtr2_tmp = i32triangVertPtr2(id);
+    i32triangVertPtr3_tmp = i32triangVertPtr3(id);
+
+    % Get triangle vertices positions
+    dTmpTriangleVertices = coder.nullcopy(zeros(3, 3));
+
+    if all(i32triangVertPtr1_tmp ~= ui32PointsIdx(idL) & i32triangVertPtr2_tmp ~= ui32PointsIdx(idL) & ...
+            i32triangVertPtr3_tmp ~= ui32PointsIdx(idL))
+
+        dTmpTriangleVertices(1:3, 1) = dVerticesPos(:, i32triangVertPtr1_tmp );
+        dTmpTriangleVertices(1:3, 2) = dVerticesPos(:, i32triangVertPtr2_tmp );
+        dTmpTriangleVertices(1:3, 3) = dVerticesPos(:, i32triangVertPtr3_tmp );
+
+        % Perform intersection test against mesh (1 ray 1 triangle) using one-sided test
+        % (cull computations excluding back-facing triangles)
+        assert(idT <= ui32NumOfTriangles)
+
+        [bLightOcclusion] = RayTriangleIntersection_MollerTrumbore(dRayOrigin, ...
+                                                                   dRayDirection,  ...
+                                                                   dTmpTriangleVertices(:, 1), ...
+                                                                   dTmpTriangleVertices(:, 2), ...
+                                                                   dTmpTriangleVertices(:, 3), ...
+                                                                   false, ...
+                                                                   true); % Shadow ray, one-sided test
+
+        if bLightOcclusion == true
+            break; % Intersection detected by Shadow Ray --> light occluded by mesh, point not illuminated
+        end
+
+    end
+
+end
+
+end
 
 
