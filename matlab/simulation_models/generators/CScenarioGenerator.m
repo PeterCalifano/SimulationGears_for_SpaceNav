d66d493aa7bcd6c2d955f968488e098c87f89e48
PeterC <petercalifano.gs@gmail.com>
2025-03-15 09:38:08 +0100

    Update hardcoded parameter

--- matlab/simulation_models/generators/CScenarioGenerator.m
+++ matlab/simulation_models/generators/CScenarioGenerator.m
@@ -437,8 +437,8 @@ classdef CScenarioGenerator < handle
                         dTargetGravityParameter = cspice_bodvrd(num2str(ui32ID),'GM',1)*1e+09;            % [m^3/(s^2)]
                     catch
                         warning('Fetch of Apophis data from kernels failed. Fallback to hardcoded data...')
-                        dTargetGravityParameter = cspice_bodvrd(num2str(ui32ID),'GM',1) * 1e9;            % [m^3/(s^2)]
                         dTargetReferenceRadius  = 1e3 * 0.175930344; % [m] ACHTUNG: Value used for Gravity SH expansion!
+                                                dTargetGravityParameter = 3.003435675;            % [m^3/(s^2)]
                     end
 
                 case EnumScenarioName.Bennu_OREx
