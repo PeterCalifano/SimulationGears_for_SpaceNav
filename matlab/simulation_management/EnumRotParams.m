806828a216ea7498a7b8464db2b5b3f782e94d4d
PeterC <petercalifano.gs@gmail.com>
2025-03-22 18:04:07 +0100

    Add new rotation params

--- matlab/simulation_management/EnumRotParams.m
+++ matlab/simulation_management/EnumRotParams.m
@@ -1,6 +1,7 @@
 classdef EnumRotParams
     enumeration
         QUAT_VSRPplus % Vector Scalar Right (chain) Passive Plus sign
+        QUAT_SVRPplus % Scalar Vector Right (chain) Passive Plus sign
         DCM
         MRP
     end
