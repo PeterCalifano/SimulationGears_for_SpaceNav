18f233e32893ccda4667646d1126e4f1ab224c53
PeterC <petercalifano.gs@gmail.com>
2025-05-02 00:02:58 +0200

    Minor change

--- matlab/simulation_models/dynamics/RHS_2BP.m
+++ matlab/simulation_models/dynamics/RHS_2BP.m
@@ -1,6 +1,6 @@
-function [dxdt] = RHS_2BP(~, x, i_strDynParams)
+function [dxdt] = RHS_2BP(~, x, strDynParams)
 
 dxdt = [x(4:6);
-    -i_strDynParams.dGravParam/norm(x(1:3))^3 * x(1:3)];
+    -strDynParams.dGravParam/norm(x(1:3))^3 * x(1:3)];
 
 end
