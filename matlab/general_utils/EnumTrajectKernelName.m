a63daa3764b91dee2a2eb605f62fe7950cded3f8
PeterC <petercalifano.gs@gmail.com>
2025-06-25 19:12:29 +0200

    Add enumeration class for trajectory names

---
+++ matlab/general_utils/EnumTrajectKernelName.m
@@ -0,0 +1,21 @@
+classdef EnumTrajectKernelName
+enumeration
+        RTO_4t1_J11p0
+        RTO_3t1_J11p0
+        RTO_3t1_J6p5
+        RTO_3t1_J9p0   
+        RTO_4t1_J13p0  
+        SSTO_0p8  
+        SSTO_1p2  
+        RTO_3t1_J13p0    
+        RTO_4t1_J9p0  
+        SSTO_1p0  
+        SSTO_1p4  
+        SSTO_1p8  
+        SSTO_1p6
+        RAMSES_RCS1_20290312_20290511_v0
+        FUT_h500
+        FUT_h600
+end
+end
+
