0af078c2406239a5fa4fc373f00419431743239d
PeterC <petercalifano.gs@gmail.com>
2025-07-20 17:50:48 +0200

    [MAJOR] Upgrade factory function to define environment properties
    - Add functionality to allow assignment of SC data (defaults to hardcoded values)
    
    - Add implementation to load 3rd bodies data from dataset object (all data + structs for EphemeridesDataFactory)

--- matlab/simulation_models/generators/builders/DefineEnvironmentProperties.m
+++ matlab/simulation_models/generators/builders/DefineEnvironmentProperties.m
@@ -1,4 +1,4 @@
-function [strDynParams, strAdditionalData] = DefineEnvironmentProperties(dEphemeridesTimegrid, ...
+function [strDynParams, strMainBodyRefData, str3rdBodyRefData] = DefineEnvironmentProperties(dEphemeridesTimegrid, ...
                                                                           enumScenarioName, ...
                                                                           charInertialFrame, ...
                                                                           kwargs)
@@ -8,7 +8,8 @@ arguments
     charInertialFrame   (1,:) char {mustBeA(charInertialFrame, ["string", "char"])} = "J2000"
 end
 arguments
-    kwargs.strDynParams (1,1) {isstruct} = struct() % To provide input struct
+    kwargs.strDynParams (1,1) {isstruct} = struct()         % Initialization value
+    kwargs.str3rdBodyRefData (1,1) {isstruct} = struct()    % Initialization value
     kwargs.bAddNonSphericalGravityCoeffs (1,1) logical {islogical, isscalar} = false;
     kwargs.objDataset = SReferenceMissionDesign()
     kwargs.charSpherHarmCoeffInputFileName (1,:) string {mustBeA(kwargs.charSpherHarmCoeffInputFileName, ["string", "char"])} = ""
@@ -44,15 +45,16 @@ end
 %% CHANGELOG
 % 19-02-2025    Pietro Califano     First version copy-pasting previous implementation
 % 14-03-2025    Pietro Califano     Move code to CScenarioGenerator static method for standardization
-% -------------------------------------------------------------------------------------------------------------
+% 21-07-2025    Pietro Califano     Add support for 3rd body reference data and generalize implementation
 %% DEPENDENCIES
-% [-]
-% -------------------------------------------------------------------------------------------------------------
-%% Future upgrades
 % [-]
 % -------------------------------------------------------------------------------------------------------------
 
 strDynParams = kwargs.strDynParams;
+str3rdBodyRefData = kwargs.str3rdBodyRefData;
+if length(fieldnames(strDynParams)) 
+    strDynParams.strSCdata = []; 
+end
 
 % Checks and info printing
 if kwargs.objDataset.bDefaultConstructed
@@ -65,6 +67,7 @@ end
                                                                                 "bAddNonSphericalGravityCoeffs", kwargs.bAddNonSphericalGravityCoeffs, ...
                                                                                 "charSpherHarmCoeffInputFileName", kwargs.charSpherHarmCoeffInputFileName);
 
+
 if kwargs.objDataset.bDefaultConstructed
 
     if max(dEphemeridesTimegrid) < 1e8
@@ -72,38 +75,92 @@ if kwargs.objDataset.bDefaultConstructed
     end
 
     % Get target fixed frame attitude wrt Inertial frame    
-    strAdditionalData.dDCM_INfromTB = cspice_pxform(char(charTargetFixedFrame), char(charInertialFrame), dEphemeridesTimegrid);
+    strMainBodyRefData.dDCM_INfromTB = cspice_pxform(char(charTargetFixedFrame), char(charInertialFrame), dEphemeridesTimegrid);
 
     % Get Sun position in Inertial frame
-    strAdditionalData.dSunPosition_IN = 1000 * cspice_spkpos('SUN', dEphemeridesTimegrid, ...
+    strMainBodyRefData.dSunPosition_IN = 1000 * cspice_spkpos('SUN', dEphemeridesTimegrid, ...
                                                 charInertialFrame, 'none', charTargetName);
 
-    % Get data from objDataset
+    % Get additional bodies data if provided (Sun not included)
+    ui32NumOfAdditionalBodies = length(kwargs.objDataset.cellAdditionalBodiesNames);
+    bAdd3rdBodiesAttitude = not(isempty(kwargs.objDataset.cellAdditionalBodiesDCM_TBfromW));
+
+    for idB = 1:ui32NumOfAdditionalBodies
+
+        if isempty(kwargs.objDataset.cellAdditionalBodiesPos_W{idB})
+            % Store data in struct for ephemerides factory
+            str3rdBodyRefData(idB).strOrbitData.dPosition_W  = kwargs.objDataset.cellAdditionalBodiesPos_W{idB};
+        end
+        
+        if bAdd3rdBodiesAttitude && not(isempty(kwargs.objDataset.cellAdditionalBodiesDCM_TBfromW{idB}))
+            str3rdBodyRefData(idB).strAttData.dDCM_WfromTB = pagetranspose(kwargs.objDataset.cellAdditionalBodiesDCM_TBfromW{idB});
+        end
+
+        try
+            charTargetName = kwargs.objDataset.cellAdditionalBodiesNames{idB};
+            fprintf('\nLoading additional body %s data from CScenarioGenerator database...', charTargetName);
+            [charTargetName, charTargetFixedFrame, strDynParams_3rdBody] = CScenarioGenerator.LoadDefaultScenarioData(charTargetName);
+
+            % Add GM information if available to scenario generator
+            strDynParams.strBody3rdData(idB+1).dGM        = strDynParams_3rdBody.strMainData.dGM;
+            strDynParams.strBody3rdData(idB+1).dRefRadius = strDynParams_3rdBody.strMainData.dRefRadius;
+
+        catch ME
+            warning('ERROR occurred while assigning data of 3rd bodies: %s.\nBody %s not available in CScenarioGenerator. GM will be set to zero.', string(ME.message), charTargetName);
+            % If not available, set to zero
+            strDynParams.strBody3rdData(idB+1).dGM = 0.0;
+            strDynParams.strBody3rdData(idB+1).dRefRadius = 0.0;
+        end
+    end
+    
 
 else
     % Load ephemeris data from dataset
     assert(length(dEphemeridesTimegrid) == length(kwargs.objDataset.dTimestamps), ...
             "ERROR: objDataset timestamps do not match specified dEphemerisTimegrid.")
 
-    strAdditionalData.dDCM_INfromTB    = pagetranspose(kwargs.objDataset.dDCM_TBfromW);
-    strAdditionalData.dSunPosition_IN  = kwargs.objDataset.dSunPosition_W;
+    strMainBodyRefData.dDCM_INfromTB    = pagetranspose(kwargs.objDataset.dDCM_TBfromW);
+    strMainBodyRefData.dSunPosition_IN  = kwargs.objDataset.dSunPosition_W;
 end
 
 
 %% Solar Radiation Pressure data
 % ACHTUNG: Make sure that unit of measure for distance matches.
-dDistFromSunAU = mean(vecnorm(strAdditionalData.dSunPosition_IN, 2, 1), "all") / 150e9; % Input distance in [m]
+dDistFromSunAU = mean(vecnorm(strMainBodyRefData.dSunPosition_IN, 2, 1), "all") / 150e9; % Input distance in [m]
 strDynParams.strSRPdata.dP_SRP0 = 1367/299792458 * (1/(dDistFromSunAU)^2); % [N/m^2]
 fprintf('\nAverage distance from the SUN in AU: %3.4f AU\n', dDistFromSunAU);
 
-% Get Sun gravity parameter
-strDynParams.strBody3rdData(1).dGM = 1E+09 * cspice_bodvrd('SUN', 'GM', 1); % Output is in km^3/s^2
+% Add Sun gravity parameter
+strDynParams.strBody3rdData(1).dGM = 1E+09 * cspice_bodvrd('SUN', 'GM', 1); % Output is in m^3/s^2
 % strDynParams.strBody3rdData(1).dGM = cspice_bodvrd('SUN', 'GM', 1); % Output is in km^3/s^2
 
 %% Spacecraft data
-strDynParams.strSCdata.dReflCoeff = 1.29;  % Global CR
-strDynParams.strSCdata.dSCmass    = 14.8; % 12; % [kg]
-strDynParams.strSCdata.dA_SRP     = 0.5329; % [m^2]
+dDefaultReflCoeff = 1.29;  % Global CR
+dDefaultSCmass    = 14.8; % 12; % [kg]
+dDefaultA_SRP     = 0.5329; % [m^2]
+
+if isempty(strDynParams.strSCdata)
+    warning('No spacecraft data provided in strDynParams.strSCdata. Default hardcoded values (RCS-1) will be used.')    
+    strDynParams.strSCdata.dReflCoeff = dDefaultReflCoeff;
+    strDynParams.strSCdata.dSCmass    = dDefaultSCmass;
+    strDynParams.strSCdata.dA_SRP     = dDefaultA_SRP;
+
+else
+    % Check for missing values and set defaults where needed
+    if not(isfield(strDynParams.strSCdata, "dReflCoeff"))
+        warning('No spacecraft reflectivity coefficient provided in strDynParams.strSCdata.dReflCoeff. Default value of 1.29 will be used.')
+        strDynParams.strSCdata.dReflCoeff = dDefaultReflCoeff;
+    end
+    if not(isfield(strDynParams.strSCdata, "dSCmass"))
+        warning('No spacecraft mass provided in strDynParams.strSCdata.dSCmass. Default value of 14.8 kg will be used.')
+        strDynParams.strSCdata.dSCmass = dDefaultSCmass;
+    end
+    if not(isfield(strDynParams.strSCdata, "dA_SRP"))
+        warning('No spacecraft area provided in strDynParams.strSCdata.dA_SRP. Default value of 0.5329 m^2 will be used.')
+        strDynParams.strSCdata.dA_SRP = dDefaultA_SRP;
+    end
+end
+
 
 end
 
