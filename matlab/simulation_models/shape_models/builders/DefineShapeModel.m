6ea44b148e2eca6c5a46f6d607703b48717f9231
PeterC <petercalifano.gs@gmail.com>
2025-08-29 17:28:07 +0200

    Extend DefineShapeModel to allow selection of km or m

--- matlab/simulation_models/shape_models/builders/DefineShapeModel.m
+++ matlab/simulation_models/shape_models/builders/DefineShapeModel.m
@@ -9,8 +9,9 @@ arguments
     charBpyRootPath     (1,:) string = fullfile(getenv("HOME"), "devDir/rendering-sw/corto_PeterCdev")
 end
 arguments
-    options.bVertFacesOnly  (1,1) {islogical} = true;
-    options.bLoadShapeModel (1,1) {islogical} = true;
+    options.bVertFacesOnly          (1,1) {islogical} = true;
+    options.bLoadShapeModel         (1,1) {islogical} = true;
+    options.charOutputLengthUnits   (1,:) char {mustBeMember(options.charOutputLengthUnits, ["km", "m"])} = "m"
 end
 %% SIGNATURE
 % [objShapeModel, strBpyCommManagerPaths] = DefineShapeModel(enumTargetName, charDataRootPath, options)
@@ -18,19 +19,16 @@ end
 %% DESCRIPTION
 % Function defining a general purpose shape model object from the specified target name as CShapeModel in 
 % SimulationGears repository (https://github.com/PeterCalifano/SimulationGears_for_SpaceNav). 
-% Paths to models forBlenderPyCommManager class are also defined (ACHTUNG: currently HARDCODED).
+% Paths to models for BlenderPyCommManager class are also defined (ACHTUNG: currently HARDCODED).
 % -------------------------------------------------------------------------------------------------------------
 %% INPUT
-% arguments
-%     enumTargetName      (1,:) {mustBeA(enumTargetName, ["string", "char", "EnumScenarioName"]), ...
-%         mustBeMember(enumTargetName, ["Apophis", "Itokawa", "Bennu", "Moon"])}
-%     charDataRootPath    (1,:) string = fullfile(getenv("HOME"), "devDir/nav-backend/simulationCodes/data/SPICE_kernels")
-%     charBpyRootPath     (1,:) string = fullfile(getenv("HOME"), "devDir/rendering-sw/corto_PeterCdev")
-% end
-% arguments
-%     options.bVertFacesOnly (1,1) {islogical} = true;
-%     options.bLoadShapeModel (1,1) {islogical} = true;
-% end
+% enumTargetName      (1,:) {mustBeA(enumTargetName, ["string", "char", "EnumScenarioName"]), ...
+%                            mustBeMember(enumTargetName, ["Apophis", "Itokawa", "Bennu", "Moon"])}
+% charDataRootPath                  (1,:) string = fullfile(getenv("HOME"), "devDir/nav-backend/simulationCodes/data/SPICE_kernels")
+% charBpyRootPath                   (1,:) string = fullfile(getenv("HOME"), "devDir/rendering-sw/corto_PeterCdev")
+% options.bVertFacesOnly            (1,1) {islogical} = true;
+% options.bLoadShapeModel           (1,1) {islogical} = true;
+% options.charOutputLengthUnits     (1,:) char {mustBeMember(options.charOutputLengthUnits, ["km", "m"])} = "m"
 % -------------------------------------------------------------------------------------------------------------
 %% OUTPUT
 % objShapeModel
@@ -39,6 +37,7 @@ end
 %% CHANGELOG
 % 10-04-2025    Pietro Califano     Update of paths definition
 % 03-05-2025    Pietro Califano     Minor revision, add Moon setup
+% 25-08-2025    Pietro Califano     Extend function to work with km and meters based on input options
 % -------------------------------------------------------------------------------------------------------------
 %% DEPENDENCIES
 % [-]
@@ -57,6 +56,15 @@ assert(contains(charUsrName, "peter") || contains(string(charUsrName(1:end-1)), 
 
 charCallDir = pwd;
 
+if strcmpi(options.charOutputLengthUnits, "km")
+    dLengthScaleCoeff       = 1.0;
+    dInvLengthScaleCoeff    = 1000.0;
+
+elseif strcmpi(options.charOutputLengthUnits, "m")
+    dLengthScaleCoeff        = 1000.0;
+    dInvLengthScaleCoeff     = 1/1000.0;
+end
+
 switch enumTargetName
 
     case "Apophis"
@@ -68,17 +76,17 @@ switch enumTargetName
         
         % Define shape model object
         objShapeModel = CShapeModel('file_obj', fullfile(path_to_shape_models, "apophis_v233s7_vert2_new.mod.obj"), ...
-            'km', 'm', options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
+            'km', options.charOutputLengthUnits, options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
 
         try
             ui32ID = 20099942;
-            objShapeModel.dObjectReferenceSize  = 1E3 * mean(cspice_bodvrd(num2str(ui32ID),'RADII',3)); % [m] ACHTUNG: Value used for Gravity SH expansion!
+            objShapeModel.dObjectReferenceSize  = dLengthScaleCoeff * mean(cspice_bodvrd(num2str(ui32ID),'RADII',3)); % [m] ACHTUNG: Value used for Gravity SH expansion!
         catch
             warning('Fetch of Apophis data from kernels failed. Fallback to hardcoded data...')
-            objShapeModel.dObjectReferenceSize  = 1E3 * 0.175930344; % [m] ACHTUNG: Value used for Gravity SH expansion!
+            objShapeModel.dObjectReferenceSize  = dLengthScaleCoeff * 0.175930344; % [m] ACHTUNG: Value used for Gravity SH expansion!
         end
         
-        objShapeModel.charTargetUnitOutput = 'm';
+        objShapeModel.charTargetUnitOutput = options.charOutputLengthUnits;
 
     case "Itokawa"
 
@@ -87,9 +95,10 @@ switch enumTargetName
         % Define shape model object
         charKernelname = fullfile(charDataRootPath, 'Itokawa/dsk/hay_a_amica_5_itokawashape_v1_0_64q.bds');
 
-        objShapeModel = CShapeModel('cspice', charKernelname, 'km', 'm', options.bVertFacesOnly, char(enumTargetName));
-        objShapeModel.dObjectReferenceSize = 1E3 * 0.161915; % [m]
-        objShapeModel.charTargetUnitOutput = 'm';
+        objShapeModel = CShapeModel('cspice', charKernelname, 'km', options.charOutputLengthUnits, ...
+                                options.bVertFacesOnly, char(enumTargetName));
+        objShapeModel.dObjectReferenceSize = dLengthScaleCoeff * 0.161915; % [m] or [km]
+        objShapeModel.charTargetUnitOutput = options.charOutputLengthUnits;
 
     case "Bennu"
 
@@ -101,12 +110,12 @@ switch enumTargetName
         charKernelname = fullfile(charDataRootPath, 'Bennu_OREx/dsk/bennu_g_03170mm_spc_obj_0000n00000_v020.bds');
         % charKernelname = fullfile(charDataRootPath, 'Bennu_OREx/dsk/bennu_g_01680mm_alt_obj_0000n00000_v021.bds');
 
-        objShapeModel = CShapeModel('cspice', charKernelname, 'km', 'm', ...
+        objShapeModel = CShapeModel('cspice', charKernelname, 'km', options.charOutputLengthUnits, ...
                         options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
 
         % Assign reference radius
-        objShapeModel.dObjectReferenceSize = 245.03; % [m]
-        objShapeModel.charTargetUnitOutput = 'm';
+        objShapeModel.dObjectReferenceSize = 245.03 / dInvLengthScaleCoeff; % [m]
+        objShapeModel.charTargetUnitOutput = options.charOutputLengthUnits;
 
     case "Moon"
 
@@ -116,37 +125,38 @@ switch enumTargetName
         % Define shape model object
         charObjModelFilePath = fullfile(charBpyRootPath, "data/scenarios/S6_Moon/Moon.obj");
 
-        objShapeModel = CShapeModel('file_obj', charObjModelFilePath, 'km', 'm', ...
-            options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
+        objShapeModel = CShapeModel('file_obj', charObjModelFilePath, 'km', options.charOutputLengthUnits, ...
+                                options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
 
-        objShapeModel.dObjectReferenceSize = 1737.42 * 1E3; % [m]
-        objShapeModel.charTargetUnitOutput = 'm';
+        objShapeModel.dObjectReferenceSize = dLengthScaleCoeff * 1737.42; % [m] or [km]
+        objShapeModel.charTargetUnitOutput = options.charOutputLengthUnits;
 
     case "Mars"
 
         charObjModelFilePath = ""; % None for now
 
-        objShapeModel = CShapeModel('file_obj', charObjModelFilePath, 'km', 'm', ...
-            options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
+        objShapeModel = CShapeModel('file_obj', charObjModelFilePath, 'km', options.charOutputLengthUnits, ...
+                            options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
 
-        objShapeModel.dObjectReferenceSize = 3386.2 * 1E3; % [m]
-        objShapeModel.charTargetUnitOutput = 'm';
+        objShapeModel.dObjectReferenceSize = dLengthScaleCoeff * 3386.2; % [m] or [km]
+        objShapeModel.charTargetUnitOutput = options.charOutputLengthUnits;
 
         % Define shape matrix in principal TF
-        dABC = [3395428, 3395428, 3377678];
+        dABC = dLengthScaleCoeff * 1E-3 * [3395428, 3395428, 3377678]; % [m] or [km]
         objShapeModel.dTargetShapeMatrix_OF = diag([1/dABC(1)^2, 1/dABC(2)^2, 1/dABC(3)^2]);
 
+        
     case "Ceres" 
         charObjModelFilePath = ""; % None for now
 
-        objShapeModel = CShapeModel('file_obj', charObjModelFilePath, 'km', 'm', ...
-            options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
+        objShapeModel = CShapeModel('file_obj', charObjModelFilePath, 'km', options.charOutputLengthUnits, ...
+                                options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
 
-        objShapeModel.dObjectReferenceSize = 939.0/2 * 1E3; % [m]
-        objShapeModel.charTargetUnitOutput = 'm';
+        objShapeModel.dObjectReferenceSize = dLengthScaleCoeff * 939.0/2; % [m] or [km]
+        objShapeModel.charTargetUnitOutput = options.charOutputLengthUnits;
             
         % Define shape matrix in principal TF
-        dABC = 1E3 * [483.1, 481.0, 445.9];
+        dABC = dLengthScaleCoeff * 1E-3 * [483.1, 481.0, 445.9]; % [m] or [km]
         objShapeModel.dTargetShapeMatrix_OF = diag([1/dABC(1)^2, 1/dABC(2)^2, 1/dABC(3)^2]);
 
     case "Dydimos"
@@ -156,9 +166,9 @@ switch enumTargetName
         error('Not implemented yet')
     
     case "NotDefined"
-        objShapeModel = CShapeModel('file_obj', "", 'km', 'm', ...
+        objShapeModel = CShapeModel('file_obj', "", 'km', options.charOutputLengthUnits, ...
             options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
-        objShapeModel.charTargetUnitOutput = 'm';
+        objShapeModel.charTargetUnitOutput = options.charOutputLengthUnits;
         
         charBlenderModelPath = "";
         charBlenderPyInterfacePath = "";
@@ -167,7 +177,7 @@ switch enumTargetName
     otherwise
         if options.bLoadShapeModel == false
             warning('Invalid or unavailable scenario, but no loading of shape required. Returning empty shape model.');
-            objShapeModel = CShapeModel('file_obj', "", 'km', 'm', ...
+            objShapeModel = CShapeModel('file_obj', "", 'km', options.charOutputLengthUnits, ...
                             options.bVertFacesOnly, char(enumTargetName), options.bLoadShapeModel);
         else
             error('Data for selected scenarios are either not setup or unavailable.');
