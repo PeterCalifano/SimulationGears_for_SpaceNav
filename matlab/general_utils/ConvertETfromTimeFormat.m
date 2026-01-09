25582efb2249319f41adaebe67faedb34f780fe6
PeterC <petercalifano.gs@gmail.com>
2026-01-09 18:37:02 +0100

    Implement function to convert to ET time from other formats

---
+++ matlab/general_utils/ConvertETfromTimeFormat.m
@@ -0,0 +1,45 @@
+function dTimegridOut = ConvertETfromTimeFormat(dTimegridIn, charInputTimeRef)%#codegen
+arguments
+    dTimegridIn              double   {mustBeReal, mustBeFinite}
+    charInputTimeRef (1,:)   char     {coder.mustBeConst, mustBeMember(charInputTimeRef, ["ET","GPS","TYVAK_ET", "TYVAK_GPS"])}
+end
+% TYVAK reference epoch in GPS seconds, precomputed as a compile-time constant
+% dTyvakGpsReference = coder.const(cspice_unitim( cspice_str2et('06 JAN 1980 00:00:00 UTC'), ...  % TYVAK reference in ET
+%                                                               'ET', ...
+%                                                               'GPS'));
+
+dTyvakReference_ET  = coder.const(-630763148.815937);
+dTyvakReference_GPS = coder.const(-630763200);
+
+bIsAvailableSPICE = coder.const(coder.target('MATLAB') && not(isempty(which("mice"))));
+
+switch upper(charInputTimeRef)
+
+    case 'ET'
+
+        % No conversion needed
+        dTimegridOut = dTimegridIn;
+
+    case 'GPS'
+
+        if bIsAvailableSPICE
+            % Convert from GPS time to ET
+            dTimegridOut = cspice_unitim(dTimegridIn, 'GPS', 'ET');
+        else
+            warning('Conversion GPS to ET through SPICE not supported when codegen.')
+        end
+
+    case 'TYVAK_ET'
+        % Add fixed TYVAK reference (in ET)
+        dTimegridOut  = dTimegridIn + dTyvakReference_ET;
+
+    case 'TYVAK_GPS'
+        % Add fixed TYVAK reference (in GPS seconds)
+        dTimegridOut  = dTimegridIn + dTyvakReference_GPS;
+
+    otherwise
+        % Should never happen because of mustBeMember, but kept for robustness
+        assert(0, sprintf('ConvertETfromTimeFormat:InvalidInputTimeRef. Unsupported input time reference "%s".', charInputTimeRef));
+end
+
+end
