d19c970e3d44da28bd50599d88259e3a6310cb72
PeterC <petercalifano.gs@gmail.com>
2026-01-13 16:15:58 +0100

    Rename variables (function is WIP)

--- matlab/general_utils/ConvertETfromTimeFormat.m
+++ matlab/general_utils/ConvertETfromTimeFormat.m
@@ -4,11 +4,11 @@ arguments
     charInputTimeRef (1,:)   char     {coder.mustBeConst, mustBeMember(charInputTimeRef, ["ET","GPS","TYVAK_ET", "TYVAK_GPS"])}
 end
 % BUG: conversion does not seem correct
-% TYVAK reference epoch in GPS seconds, precomputed as a compile-time constant
-% dTyvakGpsReference = coder.const(cspice_unitim( cspice_str2et('06 JAN 1980 00:00:00 UTC'), 'ET','GPS'));
+% Reference epoch in GPS seconds, precomputed as a compile-time constant
+% dReferenceEpochGPS_GPS = coder.const(cspice_unitim( cspice_str2et('06 JAN 1980 00:00:00 UTC'), 'ET','GPS'));
 
-dTyvakReference_ET  = coder.const(-630763148.815937);
-dTyvakReference_GPS = coder.const(-630763200);
+dReferenceEpochGPS_ET  = coder.const(-630763148.815937);
+dReferenceEpochGPS_GPS = coder.const(-630763200);
 
 bIsAvailableSPICE = coder.const(coder.target('MATLAB') && not(isempty(which("mice"))));
 
@@ -30,11 +30,11 @@ switch upper(charInputTimeRef)
 
     case 'TYVAK_ET'
         % Add fixed TYVAK reference (in ET)
-        dTimegridOut  = dTimegridIn + dTyvakReference_ET;
+        dTimegridOut  = dTimegridIn + dReferenceEpochGPS_ET;
 
     case 'TYVAK_GPS'
         % Add fixed TYVAK reference (in GPS seconds)
-        dTimegridOut  = cspice_unitim( dTimegridIn + dTyvakReference_GPS, 'GPS', 'ET');
+        dTimegridOut  = cspice_unitim( dTimegridIn + dReferenceEpochGPS_GPS, 'GPS', 'ET');
 
     otherwise
         % Should never happen because of mustBeMember, but kept for robustness
